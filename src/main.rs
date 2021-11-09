a65574d8e39ad5746a6eed324e9606c34bea3e6d
Alexander Nookinov <anookinov@gmail.com>
2021-11-09 14:57:29 +0700

    refactor into player_spawn function and add startup stage

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,16 @@ const PLAYER_SPRITE: &str = "player_a_01.png";
 
 // Entity, Component, System, Resource
 
+// region: Resouorces
+pub struct Materials {
+    player_materials: Handle<ColorMaterial>,
+}
+struct WinSize {
+    w: f32,
+    h: f32,
+}
+// endregion: Resources
+
 fn main() {
     App::build()
         .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
@@ -17,6 +27,10 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
+        .add_startup_stage(
+            "game_setup_actors",
+            SystemStage::single(player_spawn.system()),
+        )
         .run();
 }
 
@@ -26,17 +40,29 @@ fn setup(
     mut materials: ResMut<Assets<ColorMaterial>>,
     mut windows: ResMut<Windows>,
 ) {
+    let mut window = windows.get_primary_mut().unwrap();
+
     // camera
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
-    // position window
-    let mut window = windows.get_primary_mut().unwrap();
-    window.set_position(IVec2::new(0, 0));
+    // create the main resources
+    commands.insert_resource(Materials {
+        player_materials: materials.add(asset_server.load(PLAYER_SPRITE).into()),
+    });
+    commands.insert_resource(WinSize {
+        w: window.width(),
+        h: window.height(),
+    });
 
+    // position window
+    window.set_position(IVec2::new(0, 0));
+}
+
+fn player_spawn(mut commands: Commands, materials: Res<Materials>, win_size: Res<WinSize>) {
     // spawn a sprite
-    let bottom = -window.height() / 2.;
+    let bottom = -win_size.h / 2.;
     commands.spawn_bundle(SpriteBundle {
-        material: materials.add(asset_server.load(PLAYER_SPRITE).into()),
+        material: materials.player_materials.clone(),
         transform: Transform {
             translation: Vec3::new(0., bottom + 75. / 4. + 5., 10.),
             scale: Vec3::new(0.5, 0.5, 1.),
