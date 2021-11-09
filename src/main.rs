b0c9ba5c130e877ae6f42f76fe10e6c7e09c99c6
Alexander Nookinov <anookinov@gmail.com>
2021-11-09 11:29:07 +0700

    add App and setup system

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+#![allow(unused)]
+
+use bevy::{prelude::*, window};
+
+const PLAYER_SPRITE: &str = "player_a_01.png";
+
+// Entity, Component, System, Resource
+
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .insert_resource(WindowDescriptor {
+            title: "Rust Invaders!".to_string(),
+            width: 598.0,
+            height: 676.0,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup.system())
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut windows: ResMut<Windows>,
+) {
+    // camera
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    // position window
+    let mut window = windows.get_primary_mut().unwrap();
+    window.set_position(IVec2::new(0, 0))
 }
