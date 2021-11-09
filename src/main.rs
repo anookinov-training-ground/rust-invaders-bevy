9de8ee417d0e2e011badc9f055b4da2e7760caf1
Alexander Nookinov <anookinov@gmail.com>
2021-11-09 15:56:16 +0700

    extract player into a separated file and refactor main app to use PlayerPlugin

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 #![allow(unused)]
 
-use std::default;
+mod player;
 
-use bevy::{ecs::query, prelude::*, transform, window};
+use bevy::prelude::*;
+use player::PlayerPlugin;
 
 const PLAYER_SPRITE: &str = "player_a_01.png";
 const LASER_SPRITE: &str = "laser_a_01.png";
+const SCALE: f32 = 0.5;
 const TIME_STEP: f32 = 1. / 60.;
 
 // Entity, Component, System, Resource
@@ -44,14 +46,8 @@ fn main() {
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
+        .add_plugin(PlayerPlugin)
         .add_startup_system(setup.system())
-        .add_startup_stage(
-            "game_setup_actors",
-            SystemStage::single(player_spawn.system()),
-        )
-        .add_system(player_movement.system())
-        .add_system(player_fire.system())
-        .add_system(laser_movement.system())
         .run();
 }
 
@@ -78,90 +74,4 @@ fn setup(
 
     // position window
     window.set_position(IVec2::new(0, 0));
-}
-
-fn player_spawn(mut commands: Commands, materials: Res<Materials>, win_size: Res<WinSize>) {
-    // spawn a sprite
-    let bottom = -win_size.h / 2.;
-    commands
-        .spawn_bundle(SpriteBundle {
-            material: materials.player.clone(),
-            transform: Transform {
-                translation: Vec3::new(0., bottom + 75. / 4. + 5., 10.),
-                scale: Vec3::new(0.5, 0.5, 1.),
-                ..Default::default()
-            },
-            ..Default::default()
-        })
-        .insert(Player)
-        .insert(PlayerReadyFire(true))
-        .insert(Speed::default());
-}
-
-fn player_movement(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&Speed, &mut Transform, With<Player>)>,
-) {
-    if let Ok((speed, mut transform, _)) = query.single_mut() {
-        let dir = if keyboard_input.pressed(KeyCode::Left) {
-            -1.
-        } else if keyboard_input.pressed(KeyCode::Right) {
-            1.
-        } else {
-            0.
-        };
-        transform.translation.x += dir * speed.0 * TIME_STEP;
-    }
-}
-
-fn player_fire(
-    mut commands: Commands,
-    kb: Res<Input<KeyCode>>,
-    materials: Res<Materials>,
-    mut query: Query<(&Transform, &mut PlayerReadyFire, With<Player>)>,
-) {
-    if let Ok((player_tf, mut ready_fire, _)) = query.single_mut() {
-        if ready_fire.0 && kb.pressed(KeyCode::Space) {
-            let x = player_tf.translation.x;
-            let y = player_tf.translation.y;
-
-            let mut spawn_lasers = |x_offset: f32| {
-                commands
-                    .spawn_bundle(SpriteBundle {
-                        material: materials.laser.clone(),
-                        transform: Transform {
-                            translation: Vec3::new(x + x_offset, y + 15., 0.),
-                            ..Default::default()
-                        },
-                        ..Default::default()
-                    })
-                    .insert(Laser)
-                    .insert(Speed::default());
-            };
-
-            let x_offset = 144. / 4. - 5.;
-            spawn_lasers(x_offset);
-            spawn_lasers(-x_offset);
-
-            ready_fire.0 = false;
-        }
-
-        if kb.just_released(KeyCode::Space) {
-            ready_fire.0 = true;
-        }
-    }
-}
-
-fn laser_movement(
-    mut commands: Commands,
-    win_size: Res<WinSize>,
-    mut query: Query<(Entity, &Speed, &mut Transform, With<Laser>)>,
-) {
-    for (laser_entity, speed, mut laser_tf, _) in query.iter_mut() {
-        let translation = &mut laser_tf.translation;
-        translation.y += speed.0 * TIME_STEP;
-        if translation.y > win_size.h {
-            commands.entity(laser_entity).despawn();
-        }
-    }
 }
