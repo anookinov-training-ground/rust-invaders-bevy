bebe226328297f7291e609e5645a88f5b6077cb6
Alexander Nookinov <anookinov@gmail.com>
2021-11-09 14:45:47 +0700

    add transform to place and scale player entity

--- src/main.rs
+++ src/main.rs
@@ -34,8 +34,14 @@ fn setup(
     window.set_position(IVec2::new(0, 0));
 
     // spawn a sprite
+    let bottom = -window.height() / 2.;
     commands.spawn_bundle(SpriteBundle {
         material: materials.add(asset_server.load(PLAYER_SPRITE).into()),
+        transform: Transform {
+            translation: Vec3::new(0., bottom + 75. / 4. + 5., 10.),
+            scale: Vec3::new(0.5, 0.5, 1.),
+            ..Default::default()
+        },
         ..Default::default()
     });
 }
