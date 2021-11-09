36f6a79bc45b226acccce7d3ea8c65fdcaa07435
Alexander Nookinov <anookinov@gmail.com>
2021-11-09 17:11:01 +0700

    create enemy file containing enemy plugin and spawn function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-#![allow(unused)]
+// #![allow(unused)]
 
+mod enemy;
 mod player;
 
 use bevy::prelude::*;
+use enemy::EnemyPlugin;
 use player::PlayerPlugin;
 
 const PLAYER_SPRITE: &str = "player_a_01.png";
 const LASER_SPRITE: &str = "laser_a_01.png";
+const ENEMY_SPRITE: &str = "enemy_a_01.png";
 const SCALE: f32 = 0.5;
 const TIME_STEP: f32 = 1. / 60.;
 
@@ -16,17 +19,21 @@ const TIME_STEP: f32 = 1. / 60.;
 pub struct Materials {
     player: Handle<ColorMaterial>,
     laser: Handle<ColorMaterial>,
+    enemy: Handle<ColorMaterial>,
 }
 struct WinSize {
+    #[allow(unused)]
     w: f32,
     h: f32,
 }
+struct ActiveEnemies(u32);
 // endregion: Resources
 
 // region: Components
 struct Player;
 struct PlayerReadyFire(bool);
 struct Laser;
+struct Enemy;
 
 struct Speed(f32);
 impl Default for Speed {
@@ -45,8 +52,10 @@ fn main() {
             height: 676.0,
             ..Default::default()
         })
+        .insert_resource(ActiveEnemies(0))
         .add_plugins(DefaultPlugins)
         .add_plugin(PlayerPlugin)
+        .add_plugin(EnemyPlugin)
         .add_startup_system(setup.system())
         .run();
 }
@@ -57,7 +66,7 @@ fn setup(
     mut materials: ResMut<Assets<ColorMaterial>>,
     mut windows: ResMut<Windows>,
 ) {
-    let mut window = windows.get_primary_mut().unwrap();
+    let window = windows.get_primary_mut().unwrap();
 
     // camera
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
@@ -66,6 +75,7 @@ fn setup(
     commands.insert_resource(Materials {
         player: materials.add(asset_server.load(PLAYER_SPRITE).into()),
         laser: materials.add(asset_server.load(LASER_SPRITE).into()),
+        enemy: materials.add(asset_server.load(ENEMY_SPRITE).into()),
     });
     commands.insert_resource(WinSize {
         w: window.width(),
